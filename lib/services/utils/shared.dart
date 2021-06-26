3248c34f45d5fbd61b13b93f9b687e1ea053ede6
jmanga2020 <jackmanga40@gmail.com>
2021-06-26 15:58:53 +0300

    updated write|set methods -> String

--- lib/services/utils/shared.dart
+++ lib/services/utils/shared.dart
@@ -1,9 +1,10 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class TempMemory {
-
   /* STORED VALUES
   splash
+  regions
+  region
   */
 
   ///method to write to local memory [boolean]
@@ -12,9 +13,21 @@ class TempMemory {
     _prefs.setBool(key, value);
   }
 
+  ///method to write to local memory [String]
+  static Future<void> writeString({String key, String value}) async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    _prefs.setString(key, value);
+  }
+
   ///method to get from local memory [boolean]
   static Future<bool> getBool({String key}) async {
     SharedPreferences _prefs = await SharedPreferences.getInstance();
     return _prefs.getBool(key) ?? true;
+  }
+
+  ///method to get from local memory [String]
+  static Future<String> getString({String key}) async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    return _prefs.getString(key) ?? '';
   }
 }
