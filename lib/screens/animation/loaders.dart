e621a4eef7292357d6ef6a12b6031f25a9ca6531
jmanga2020 <jackmanga40@gmail.com>
2021-06-26 12:43:27 +0300

    added animation loaders

---
+++ lib/screens/animation/loaders.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class ThreeBounceLoader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SpinKitThreeBounce(
+      color: Colors.white,
+      size: 50.0,
+    );
+  }
+}
